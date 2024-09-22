5fef892683ac0cadcf6d05adbda5e317872f4671
jimmyberg <jimmyberg.kim@gmail.com>
2024-09-22 15:18:44 +0900

    chore: account transaction study repository 코드 정리

--- spring-jpa/src/main/kotlin/me/jimmyberg/springjpa/account/repository/study/AccountTransactionStudyRepository.kt
+++ spring-jpa/src/main/kotlin/me/jimmyberg/springjpa/account/repository/study/AccountTransactionStudyRepository.kt
@@ -2,15 +2,10 @@ package me.jimmyberg.springjpa.account.repository.study
 
 import me.jimmyberg.springjpa.account.repository.entity.AccountEntity
 import me.jimmyberg.springjpa.util.printStep
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 
 @Repository
 class AccountTransactionStudyRepository : AccountStudyRepository() {
-    // logger
-    private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun test01(accountNo: String, amount: Long): AccountEntity {
         val entity = findEntity(accountNo).printStep(1)
